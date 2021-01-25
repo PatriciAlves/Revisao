52b7ba8167cfc69eac0757d356e2c78c087696ae
Patricia Alves <patricia.18_08@hotmail.com>
2021-01-25 09:31:06 -0300

    criação das opcoes do menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+
+                switch (opcaoUsuario)
+
+                {
+                    case "1":
+                        //TODO: add alunos
+                        break;
+
+                    case "2":
+
+                        //TODO: lista alunos
+                        break;
+
+                    case "3":
+                        //TODO: Calcula a média geral
+                        break;
+
+                    default: // caso não seja nenhum desses valores
+                        throw new ArgumentOutOfRangeException();
+                        // dispara um excessao informando que o valor esta fora do "range" programado.
+                }
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno. ");
+            Console.WriteLine("2 - Listar alunos.");
+            Console.WriteLine("3 - Calcular media geral.");
+            Console.WriteLine("X - Sair.");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
         }
     }
 }
